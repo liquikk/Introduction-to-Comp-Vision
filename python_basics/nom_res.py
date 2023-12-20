7c9e14659b1fa15318ce3b58eda9d4174eb61174
liquikk <107905363+liquikk@users.noreply.github.com>
2023-12-20 23:07:01 +0800

    ( ▀ ͜͞ʖ▀)

--- python_basics/nom_res.py
+++ python_basics/nom_res.py
@@ -5,7 +5,13 @@ def calculate_resolution(img):
     image = np.loadtxt(img, dtype=int, skiprows=1)
     max_size = np.loadtxt(img, dtype=int, max_rows=1)
     resolution = max_size / image.shape[1]
+
+    if np.all(image == 0):
+        print(f"На изображении {img} - отсутствует фигура")
+        return None
+
     return resolution
+
 
 image_files = ["txts/figure1.txt", "txts/figure2.txt", "txts/figure4.txt", "txts/figure5.txt", "txts/figure6.txt"]
 
